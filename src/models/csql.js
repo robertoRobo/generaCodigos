56a3b663e89cd741e109d773b546414cacf6a314
roberto <robert.tito94@hotmail.com>
2018-05-17 13:25:43 -0500

    se cambio la ubicacion de la BD

--- src/models/csql.js
+++ src/models/csql.js
@@ -1,7 +1,7 @@
 connection = mysql.createConnection({
-	host:'192.168.56.101',
-	user:'root',
-	password: 'roberto123',
-	database: 'taqueria'
+	host:'sql3.freemysqlhosting.net',
+	user:'sql3238525',
+	password: 'xqhkqicZdD',
+	database: 'sql3238525'
 });
 module.exports = connection;
